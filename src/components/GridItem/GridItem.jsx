b2d48d6afbcc06eda2253347155c1e49f483a72e
David Martinez <imdj9003@gmail.com>
2022-04-20 13:25:35 -0400

    ItemGrid responsive full

--- src/components/GridItem/GridItem.jsx
+++ src/components/GridItem/GridItem.jsx
@@ -1,17 +1,24 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/function-component-definition */
 import { Box, Typography } from "@mui/material";
+import ItemGrid from "components/ItemGrid/ItemGrid";
 import React from "react";
 
 // Import css Componet
 import "./grid-item.css";
 
+const items = [1, 2, 3, 4, 5, 6, 7, 8];
+
 const GridItem = () => {
   return (
     <Box className="grid-item__container">
-      <Typography variant="body1">20 Resultados</Typography>
+      <Typography variant="body1" sx={{ ml: "2rem" }}>
+        20 Resultados
+      </Typography>
       <Box className="grid-item__items-container">
-        <Typography variant="body1">Aqui va texto</Typography>
+        {items.map((i) => (
+          <ItemGrid key={i} />
+        ))}
       </Box>
     </Box>
   );
--- src/components/ItemGrid/ItemGrid.jsx
+++ src/components/ItemGrid/ItemGrid.jsx
@@ -1,12 +1,39 @@
 /* eslint-disable arrow-body-style */
 /* eslint-disable react/function-component-definition */
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from "@mui/material";
 import React from "react";
-
 // Import Css Component
 import "./item-grid.css";
 
+// Img import
+import CardImage from "../../assets/images/bruce-mars.jpg";
+
 const ItemGrid = () => {
-  return <div>ItemGrid</div>;
+  return (
+    <Card sx={{ maxWidth: 325, border: "1px outset #fbac33", borderRadius: "10px", m: 2 }}>
+      <CardMedia
+        component="img"
+        height="180"
+        image={CardImage}
+        alt="green iguana"
+        sx={{ borderRadius: "0 0 8px 8px" }}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="body1" component="div">
+          Lizard
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+          across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions sx={{ display: "flex", justifyContent: "flex-end" }}>
+        <Button variant="contained" size="small" sx={{ textTransform: "capitalize" }}>
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
 };
 
 export default ItemGrid;
