3d669d5c9449c5958e2699f91a0e5f78e911f3ec
sito8943 <sito8943@gmail.com>
2022-04-20 17:47:33 -0400

    calendar

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -4,11 +4,14 @@
 import React, { useEffect, useState } from "react";
 
 // @mui components
-import { Box, Paper, Typography } from "@mui/material";
+import { useTheme, Box, Paper, Typography } from "@mui/material";
 
 // own components
 import Container from "components/Container/Container";
 import Accordion from "components/Accordion/Accordion";
+
+// contexts
+import { useLanguage } from "context/LanguageProvider";
 
 // styles
 import "./style.css";
@@ -18,27 +21,58 @@ const CCalendar = () => {
   const [month, setMonth] = useState(new Date().getMonth());
   const [year, setYear] = useState(new Date().getFullYear());
 
+  const { languageState } = useLanguage();
+  const theme = useTheme();
+
   const events = [
-    { title: "Hola", details: ["Hola", "Sito"] },
-    { title: "Sito", details: ["Hola", "Sito"] },
+    { title: "Evento1", date: new Date(2022, 4, 1), details: ["Hola", "Sito"] },
+    { title: "Evento2", date: new Date(2022, 4, 12), details: ["Hola", "Sito"] },
+    { title: "Evento3", date: new Date(2022, 4, 10), details: ["Hola", "Sito"] },
+    { title: "Evento4", date: new Date(2022, 4, 5), details: ["Hola", "Sito"] },
+    { title: "Evento5", date: new Date(2022, 4, 9), details: ["Hola", "Sito"] },
   ];
 
   const arrayOfDays = () => {
-    const aDays = [];
-    for (let i = 0; i < days; i += 1) aDays.push(i + 1);
+    const aDays = [[], [], [], [], [], [], []];
+    for (let i = 0; i < days; i += 1) {
+      const tDate = new Date(year, month, i);
+      aDays[tDate.getDay()].push(i + 1);
+    }
     return aDays;
   };
 
   return (
     <Container>
-      <Typography>{}</Typography>
-      <Container sx={{ flexWrap: "wrap" }}>
+      <Typography>{languageState.texts.Activities.Calendar.Months[month]}</Typography>
+      <Container>
         {arrayOfDays().map((item, i) => (
-          <Paper key={i}>
-            {events.map((jtem, j) => (
-              <Accordion key={j} title={jtem.title} details={jtem.details} />
+          <Container direction="column">
+            {item.map((jtem, j) => (
+              <Paper
+                elevation={0}
+                key={j}
+                sx={{
+                  padding: "10px",
+                  paddingLeft: "0",
+                  minWidth: "150px",
+                  minHeight: "150px",
+                  background: "#00000000",
+                  border: `1px solid ${theme.palette.secondary.main}`,
+                  borderRadius: 0,
+                }}
+              >
+                <Typography sx={{ textAlign: "center" }}>
+                  {jtem}
+                  {events.map(
+                    (ktem, k) =>
+                      ktem.date.getDate() === jtem && (
+                        <Accordion key={k} title={ktem.title} details={ktem.details} />
+                      )
+                  )}
+                </Typography>
+              </Paper>
             ))}
-          </Paper>
+          </Container>
         ))}
       </Container>
     </Container>
