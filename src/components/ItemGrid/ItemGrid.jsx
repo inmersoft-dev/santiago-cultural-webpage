badbd0c5cb9681b27219599674f970dc94708165
David Martinez <imdj9003@gmail.com>
2022-04-20 12:20:30 -0400

    ItemGrid Component creation

---
+++ src/components/ItemGrid/ItemGrid.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/function-component-definition */
+import React from "react";
+
+// Import Css Component
+import "./item-grid.css";
+
+const ItemGrid = () => {
+  return <div>ItemGrid</div>;
+};
+
+export default ItemGrid;
