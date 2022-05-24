688ff4df5cccc0abafd2c478b2cbf393250787a2
sito8943 <sito8943@gmail.com>
2022-05-24 10:03:27 -0400

    News details

--- src/services/get.js
+++ src/services/get.js
@@ -4,6 +4,33 @@ import { getAuth } from "auth/auth";
 import config from "../config";
 
 /**
+ * It then makes a GET request to the server using the axios library.
+ * If the request is successful, it returns the data.
+ * If the request is unsuccessful, it returns an error.
+ * @param {string} from - The endpoint to call.
+ * @param {string} [collection] - the name of the collection to load from
+ * @param {string} [id] - the id of the object to load
+ * @returns The data returned from the server.
+ */
+export const loadFromServerGet = async (from, collection = "", id = "", lang = "") => {
+  try {
+    const response = await axios.get(
+      `${config.serverUrl}/${from}${collection !== "" ? `?collection=${collection}` : ""}${
+        id !== "" ? `&id=${id}` : ""
+      }${lang !== "" ? `&lang=${lang}` : ""}`,
+      {
+        headers: getAuth,
+      }
+    );
+    const data = await response.data;
+    if (data.error === undefined) return data;
+    return { error: response.statusText };
+  } catch (err) {
+    return { error: String(err) };
+  }
+};
+
+/**
  * @param {string} from - The name of the endpoint to load data from.
  * @returns The data returned from the server.
  */
--- src/views/Details/Details.jsx
+++ src/views/Details/Details.jsx
@@ -1,19 +1,67 @@
 /* eslint-disable react/function-component-definition */
 /* eslint-disable arrow-body-style */
 
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+// @mui icons
+import ReplayIcon from "@mui/icons-material/Replay";
+
+// @mui components
+import { Typography, IconButton } from "@mui/material";
+
 // own components
 import Container from "components/Container/Container";
-import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import Loading from "components/Loading/Loading";
+import Image from "components/Image/Image";
+
+// services
+import { loadFromServerGet } from "services/get";
+
+// contexts
+import { useLanguage } from "context/LanguageProvider";
 
 const Details = () => {
   const { data } = useParams();
 
-  useEffect(() => {
+  const { languageState } = useLanguage();
+
+  const [loading, setLoading] = useState(1);
+
+  const [object, setObject] = useState({});
+
+  const init = async () => {
     const params = data.substring(1).split("-");
     const id = params[0];
     const collection = params[1];
-    console.log(id, collection);
+    try {
+      const remoteData = await loadFromServerGet(
+        "api/load/get",
+        collection,
+        id,
+        languageState.language
+      );
+      console.log(remoteData);
+      if (remoteData.error) {
+        console.log(remoteData.error);
+        setLoading(-1);
+      } else {
+        setObject(remoteData);
+        setLoading(0);
+      }
+    } catch (e) {
+      console.log(e);
+      setLoading(-1);
+    }
+  };
+
+  const reload = () => {
+    setLoading(1);
+    init();
+  };
+
+  useEffect(() => {
+    init();
   }, []);
 
   return (
@@ -21,10 +69,49 @@ const Details = () => {
       sx={{
         width: "100vw",
         height: "100vh",
-        paddingTop: "88px",
       }}
     >
-      Hola
+      <Loading visible={loading === 1} />
+      <Container
+        sx={{
+          padding: { md: "100px 0", xs: "80px 20px" },
+          paddingLeft: { md: "10rem", xs: "20px" },
+        }}
+      >
+        {loading === 0 && (
+          <Container sx={{ width: "80%" }}>
+            <Container
+              sx={{
+                width: " 400px",
+                height: "100%",
+                padding: "25px",
+                img: {
+                  width: "350px",
+                  height: "350px",
+                  objectFit: "cover",
+                  borderRadius: "1rem",
+                },
+              }}
+            >
+              <Image img={object.headerImage.url} />
+            </Container>
+            <Container sx={{ marginTop: "20px", flex: 1, flexDirection: "column" }}>
+              <Typography variant="h5">{object.texts.title}</Typography>
+              <Typography variant="subtitle1">{object.texts.subtitle}</Typography>
+            </Container>
+          </Container>
+        )}
+        {loading === -1 && (
+          <Container justify="center" align="center" sx={{ height: "500px", width: "100%" }}>
+            <Typography sx={{ marginRight: "20px" }}>
+              {languageState.texts.Error.Connection}
+            </Typography>
+            <IconButton color="primary" onClick={reload} variant="contained">
+              <ReplayIcon />
+            </IconButton>
+          </Container>
+        )}
+      </Container>
     </Container>
   );
 };
