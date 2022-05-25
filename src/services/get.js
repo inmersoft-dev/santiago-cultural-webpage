d639fac74b7a0dc6cc02c1f2e69b8bed71c14caf
sito8943 <sito8943@gmail.com>
2022-05-25 18:17:51 -0400

    a

--- src/services/get.js
+++ src/services/get.js
@@ -7,6 +7,10 @@ import config from "../config";
  * It then makes a GET request to the server using the axios library.
  * If the request is successful, it returns the data.
  * If the request is unsuccessful, it returns an error.
+ * @param {string} from - The endpoint to call.
+ * @param {string} [collection] - the name of the collection to load from
+ * @param {string} [id] - the id of the object to load
+ * @returns The data returned from the server.
  */
 export const loadFromServerGet = async (from, collection = "", id = "", lang = "") => {
   try {
@@ -29,15 +33,12 @@ export const loadFromServerGet = async (from, collection = "", id = "", lang = "
 /**
  * @param {string} from - The name of the endpoint to load data from.
  * @returns The data returned from the server.
- */ const loadFromServerGet = async (collection = "", id = "", lang = "") => {
+ */
+export const loadFromServer = async (from) => {
   try {
-    const response = await axios.get(
-      `${config.serverUrl}/get${collection !== "" ? `?collection=${collection}` : ""}
-      ${id !== "" ? `&id=${id}` : ""}${lang !== "" ? `&lang=${lang}` : ""}`,
-      {
-        headers: getAuth,
-      }
-    );
+    const response = await axios.get(`${config.serverUrl}/${from}`, {
+      headers: getAuth,
+    });
     const data = await response.data;
     if (data.error === undefined) return data;
     return { error: response.statusText };
@@ -45,5 +46,3 @@ export const loadFromServerGet = async (from, collection = "", id = "", lang = "
     return { error: String(err) };
   }
 };
-
-export default loadFromServerGet;
